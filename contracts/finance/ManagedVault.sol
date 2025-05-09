a130dbed69a11ac9442f460eb18f7b8e531f751d
Arr00 <13561405+arr00@users.noreply.github.com>
2025-05-09 14:10:09 -0400

    add docs

--- contracts/finance/ManagedVault.sol
+++ contracts/finance/ManagedVault.sol
@@ -3,11 +3,15 @@ pragma solidity ^0.8.24;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @dev A minimalistic vault managed by the deployer.
 contract ManagedVault is Ownable {
+    error MangedVaultCallFailed();
+
     constructor() Ownable(msg.sender) {}
 
+    /// @dev Execute a given call to `to` with value `value` and calldata `data. Only callable by {owner}.
     function call(address to, uint256 value, bytes calldata data) public virtual onlyOwner {
         (bool success, ) = to.call{ value: value }(data);
-        require(success, "Call failed");
+        require(success, MangedVaultCallFailed());
     }
 }
