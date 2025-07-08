6ed57a30153ab2c1434b2fbe0751c26773f159d8
Arr00 <13561405+arr00@users.noreply.github.com>
2025-07-08 09:27:57 -0600

    change contract name and emit non-zero amount

--- contracts/token/extensions/ConfidentialFungibleTokenERC20Events.sol
+++ contracts/token/extensions/ConfidentialFungibleTokenERC20Events.sol
@@ -5,9 +5,9 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {ConfidentialFungibleToken, euint64} from "../ConfidentialFungibleToken.sol";
 
-abstract contract ConfidentialFungibleTokenERC20Events is ConfidentialFungibleToken {
+abstract contract ConfidentialFungibleTokenERC20Like is ConfidentialFungibleToken {
     function _update(address from, address to, euint64 amount) internal virtual override returns (euint64) {
-        emit IERC20.Transfer(from, to, 1);
+        emit IERC20.Transfer(from, to, 0);
         return super._update(from, to, amount);
     }
 }
