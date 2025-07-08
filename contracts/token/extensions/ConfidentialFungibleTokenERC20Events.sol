e9268c79c85470000625ffb45b4c337e8c7dff0a
Arr00 <13561405+arr00@users.noreply.github.com>
2025-07-08 11:15:06 -0600

    fix

--- contracts/token/extensions/ConfidentialFungibleTokenERC20Events.sol
+++ contracts/token/extensions/ConfidentialFungibleTokenERC20Events.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.26;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 import {ConfidentialFungibleToken, euint64} from "../ConfidentialFungibleToken.sol";
 
-abstract contract ConfidentialFungibleTokenERC20Like is ConfidentialFungibleToken {
+abstract contract ConfidentialFungibleTokenERC20Events is ConfidentialFungibleToken {
     function _update(address from, address to, euint64 amount) internal virtual override returns (euint64) {
         emit IERC20.Transfer(from, to, 0);
         return super._update(from, to, amount);
