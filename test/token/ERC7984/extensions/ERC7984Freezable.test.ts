754ecec1dc209bab72384c383841d440180b8551
Arr00 <13561405+arr00@users.noreply.github.com>
2025-08-20 14:36:35 -0400

    update tests

--- test/token/ERC7984/extensions/ERC7984Freezable.test.ts
+++ test/token/ERC7984/extensions/ERC7984Freezable.test.ts
@@ -4,7 +4,6 @@ import { FhevmType } from '@fhevm/hardhat-plugin';
 import { expect } from 'chai';
 import { ethers, fhevm } from 'hardhat';
 
-/* eslint-disable no-unexpected-multiline */
 describe('ERC7984Freezable', function () {
   async function deployFixture() {
     const [holder, recipient, freezer, operator, anyone] = await ethers.getSigners();
@@ -15,10 +14,7 @@ describe('ERC7984Freezable', function () {
 
   it('should set and get confidential frozen', async function () {
     const { token, acl, holder, recipient, freezer } = await deployFixture();
-    await token
-      .connect(holder)
-      .$_mint(recipient.address, 1000)
-      .then(tx => tx.wait());
+    await token.connect(holder).$_mint(recipient.address, 1000);
     const encryptedInput = await fhevm
       .createEncryptedInput(await token.getAddress(), freezer.address)
       .add64(100)
@@ -36,23 +32,20 @@ describe('ERC7984Freezable', function () {
       .withArgs(recipient.address, encryptedInput.handles[0]);
     const frozenHandle = await token.confidentialFrozen(recipient.address);
     expect(frozenHandle).to.equal(ethers.hexlify(encryptedInput.handles[0]));
-    expect(await acl.isAllowed(frozenHandle, recipient.address)).to.be.true;
-    expect(await fhevm.userDecryptEuint(FhevmType.euint64, frozenHandle, await token.getAddress(), recipient)).to.equal(
-      100,
-    );
+    await expect(acl.isAllowed(frozenHandle, recipient.address)).to.eventually.be.true;
+    await expect(
+      fhevm.userDecryptEuint(FhevmType.euint64, frozenHandle, await token.getAddress(), recipient),
+    ).to.eventually.equal(100);
     const balanceHandle = await token.confidentialBalanceOf(recipient.address);
-    expect(
-      await fhevm.userDecryptEuint(FhevmType.euint64, balanceHandle, await token.getAddress(), recipient),
-    ).to.equal(1000);
+    await expect(
+      fhevm.userDecryptEuint(FhevmType.euint64, balanceHandle, await token.getAddress(), recipient),
+    ).to.eventually.equal(1000);
     const confidentialAvailableArgs = recipient.address;
     const availableHandle = await token.confidentialAvailable.staticCall(confidentialAvailableArgs);
-    await (token as any)
-      .connect(recipient)
-      .confidentialAvailableAccess(confidentialAvailableArgs)
-      .then(tx => tx.wait());
-    expect(
-      await fhevm.userDecryptEuint(FhevmType.euint64, availableHandle, await token.getAddress(), recipient),
-    ).to.equal(900);
+    await (token as any).connect(recipient).confidentialAvailableAccess(confidentialAvailableArgs);
+    await expect(
+      fhevm.userDecryptEuint(FhevmType.euint64, availableHandle, await token.getAddress(), recipient),
+    ).to.eventually.equal(900);
   });
 
   it('should not set confidential frozen if not called by freezer', async function () {
@@ -78,10 +71,7 @@ describe('ERC7984Freezable', function () {
 
   it('should transfer max available', async function () {
     const { token, holder, recipient, freezer, anyone } = await deployFixture();
-    await token
-      .connect(holder)
-      .$_mint(recipient.address, 1000)
-      .then(tx => tx.wait());
+    await token.connect(holder).$_mint(recipient.address, 1000);
     const encryptedInput = await fhevm
       .createEncryptedInput(await token.getAddress(), freezer.address)
       .add64(100)
@@ -92,17 +82,13 @@ describe('ERC7984Freezable', function () {
         recipient.address,
         encryptedInput.handles[0],
         encryptedInput.inputProof,
-      )
-      .then(tx => tx.wait());
+      );
     const confidentialAvailableArgs = recipient.address;
     const availableHandle = await token.confidentialAvailable.staticCall(confidentialAvailableArgs);
-    await (token as any)
-      .connect(recipient)
-      .confidentialAvailableAccess(confidentialAvailableArgs)
-      .then(tx => tx.wait());
-    expect(
-      await fhevm.userDecryptEuint(FhevmType.euint64, availableHandle, await token.getAddress(), recipient),
-    ).to.equal(900);
+    await (token as any).connect(recipient).confidentialAvailableAccess(confidentialAvailableArgs);
+    await expect(
+      fhevm.userDecryptEuint(FhevmType.euint64, availableHandle, await token.getAddress(), recipient),
+    ).to.eventually.equal(900);
     const encryptedInput2 = await fhevm
       .createEncryptedInput(await token.getAddress(), recipient.address)
       .add64(900)
@@ -113,16 +99,15 @@ describe('ERC7984Freezable', function () {
         anyone.address,
         encryptedInput2.handles[0],
         encryptedInput2.inputProof,
-      )
-      .then(tx => tx.wait());
-    expect(
-      await fhevm.userDecryptEuint(
+      );
+    await expect(
+      fhevm.userDecryptEuint(
         FhevmType.euint64,
         await token.confidentialBalanceOf(recipient.address),
         await token.getAddress(),
         recipient,
       ),
-    ).to.equal(100);
+    ).to.eventually.equal(100);
   });
 
   it('should transfer zero if transferring more than available', async function () {
@@ -141,8 +126,7 @@ describe('ERC7984Freezable', function () {
         recipient.address,
         encryptedInput.handles[0],
         encryptedInput.inputProof,
-      )
-      .then(tx => tx.wait());
+      );
     const encryptedInput2 = await fhevm
       .createEncryptedInput(await token.getAddress(), recipient.address)
       .add64(501)
@@ -153,16 +137,14 @@ describe('ERC7984Freezable', function () {
         anyone.address,
         encryptedInput2.handles[0],
         encryptedInput2.inputProof,
-      )
-      .then(tx => tx.wait());
-    expect(
-      await fhevm.userDecryptEuint(
+      );
+    await expect(
+      fhevm.userDecryptEuint(
         FhevmType.euint64,
         await token.confidentialBalanceOf(recipient.address),
         await token.getAddress(),
         recipient,
       ),
-    ).to.equal(1000);
+    ).to.eventually.equal(1000);
   });
 });
-/* eslint-disable no-unexpected-multiline */
